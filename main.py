e2780c8e388f2ad07374a27a91a8c946fe464a68
AngelArielGomezChaparro <182712613+AngelArielGomezChaparro@users.noreply.github.com>
2024-11-05 14:05:04 -0500

    Solution exercise 4

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-voltaje1 = float(input("ingrese el primer voltaje: "))
-voltaje2 = float(input("ingrese el segundo voltaje: "))
-voltaje3 = float(input("ingrese el tercer voltaje: "))
+metros = float(input("ingrese los metros para convertirlos en kilometros: "))
 
-promedio = (voltaje1 + voltaje2 + voltaje3) / 3
+kilometros = (metros / 1000)
 
-if promedio > 220:
-    print(f"PELIGRO")
-elif promedio >= 115:
-    print(F"Alto voltaje")
-else:
-    print(f"Voltaje correcto")
+
+print(f"Sus metros en kilometros es: {kilometros}")
